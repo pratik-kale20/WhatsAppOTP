a5e0a96449d625c9398196e5571d1566f8e0fcb6
pratik-kale20 <ppvkale@gmail.com>
2022-06-29 11:41:06 +0530

    Test1

---
+++ app.py
@@ -0,0 +1,36 @@
+import flask
+from flask import request
+import requests
+import json
+
+app = flask.Flask(__name__)
+url = "https://api.interakt.ai/v1/public/message/"
+
+@app.route('/', methods = ['GET'])
+def home():
+    token = str(request.args['token'])
+    mobile = str(request.args['mobile'])
+    otp = str(request.args['otp'])
+    try: 
+        payload = json.dumps({
+        "countryCode": "+91",
+        "phoneNumber": mobile,
+        "callbackData": "Error message",
+        "type": "Template",
+        "template": {
+            "name": "verification",
+            "languageCode": "en",
+            "bodyValues": [
+            otp
+            ]
+        }
+        })
+        headers = {
+        'Authorization': 'Basic '+ token,
+        'Content-Type': 'application/json'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+        return response.text
+    except:
+        return  "Error Message"
